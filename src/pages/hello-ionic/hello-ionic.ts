078d3be4b851871ddc9c3f659e95ddaba22cdffb
Hashini <hashinips96@gmail.com>
2018-11-08 22:16:13 +0530

    Signup and Login Ui changes

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { VotePage } from '../vote/vote';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private alertctrl:AlertController) {
+
 
   }
 
+  toVote(){
+    this.navCtrl.push(VotePage);
+  }
   
 }
